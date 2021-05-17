b7c266e3f4160cf4d7f22ac88100c766331e833e
nmisustin <nmisustin@gmail.com>
2021-05-17 02:05:36 -0400

    made everything async and added enquirer

--- index.js
+++ index.js
@@ -1,1 +1,170 @@
-
+const inquirer = require("inquirer");
+const cTable = require('console.table')
+const{getAllEmployees, getDepartments, addDepartment, addRole, addEmployee, changeEmployeeRole, getRoles, closeConnection} =require('./routes/employeeRoutes');
+const inputCheck = require("../../../Downloads/12.5/utils/inputCheck");
+async function choices(){
+    const answers = await inquirer.prompt([{
+        name: 'choice',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: ['View employees', 'View roles', 'View departments', 'Add a new employee', 'Add a new role', 'Add a new department', "Change an employee's role", "I'm done!" ]
+    }])
+    return await choiceActions(answers.choice);
+}
+async function choiceActions(choice){
+    let result = true;
+    if(choice === 'View employees'){
+        const response = await  getAllEmployees();
+        console.table(response);
+    }
+    else if(choice === 'View roles' ){
+        const response =  await getRoles();
+        console.table(response);
+    }
+    else if(choice === 'View departments'){
+        const response = await getDepartments();
+        console.table(response);
+    }
+    else if(choice === 'Add a new employee'){
+        await employeeAdder();
+    }
+    else if(choice === 'Add a new role'){
+        await roleAdder();
+    }
+    else if(choice === 'Add a new department'){
+        await departmentAdder();
+    }
+    else if(choice === "Change an employee's role"){
+        await roleChanger();
+    }
+    else if(choice === "I'm done!"){
+        result = false;
+    }
+    return result;
+}
+async function employeeAdder(){
+    const answers = await inquirer.prompt([
+        {
+            name: 'first_name',
+            type: 'input',
+            message: 'What is the first name of your employee?'
+        },
+        {
+            name: 'last_name',
+            type: 'input',
+            message: 'What is the last name of your employee?'
+        },
+        {
+            name: 'role_id',
+            type: 'list',
+            message: 'What role will this employee be working in',
+            choices: await getRoleNames()
+        },
+        {
+            name: 'manager_id',
+            type: 'list',
+            message: "Who is this employee's manager?",
+            choices: await getEmployeeNames()
+        }
+    ])
+    answers.role_id = searchRoleId(answers.role_id, await getRoles());
+    answers.manager_id = searchEmployeeId(answers.manager_id, await getAllEmployees())
+    console.log(answers);
+    addEmployee(answers);
+}
+async function roleAdder(){
+    const answers = await inquirer.prompt([
+        {
+            name: 'title',
+            type: 'input',
+            message: 'What is the name of the new role?'
+        },
+        {
+            name: 'salary',
+            type: 'number',
+            message: 'What is the salary of the new role?'
+        },
+        {
+            name: 'department_id',
+            type: 'list',
+            message: 'What department is the new role a part of?',
+            choices: await getDepartmentNames()
+        } 
+    ])
+    answers.department_id = searchDepartmentId(answers.department_id, await getDepartments());
+    addRole(answers);
+}
+async function departmentAdder(){
+    const answers = await inquirer.prompt([
+        {
+            name: 'department_name',
+            type: 'input',
+            message: 'What is your new department name?'
+        }
+    ])
+    addDepartment(answers.department_name);
+}
+async function roleChanger(){
+    const answers = await inquirer.prompt([
+        {
+            name: 'id',
+            type: 'list',
+            message: "Which employee's role would you like to change?",
+            choices: await getEmployeeNames()
+        },
+        {
+            name: 'role_id',
+            type: 'list',
+            message: 'Which role would you like to give this employee?',
+            choices: await getRoleNames()
+        }
+    ])
+    answers.id = searchEmployeeId(answers.id, await getAllEmployees());
+    answers.role_id = searchRoleId(answers.role_id, await getRoles());
+    changeEmployeeRole(answers);
+}
+async function getDepartmentNames(){
+    const departments = await getDepartments()
+    const departmentNames = []
+    for(let i=0; i<departments.length; i++){
+        const name = departments[i].department_name;
+        departmentNames.push(name);
+    }
+    return departmentNames
+}
+async function getEmployeeNames(){
+   const employees = await getAllEmployees()
+   const employeeNames = []
+   for(let i=0; i<employees.length; i++){
+        const name = employees[i].employee_name;
+        employeeNames.push(name);
+    }
+    return employeeNames
+}
+async function getRoleNames(){
+    const roles = await getRoles();
+    const roleNames = [];
+    for(let i=0; i<roles.length; i++){
+        const name = roles[i].title;
+        roleNames.push(name);
+    }
+    return roleNames
+}
+function searchDepartmentId(key, array){
+    const department = array.find(d => d.department_name === key)
+    return department.id
+}
+function searchEmployeeId(key, array){
+    const employee = array.find(e => e.employee_name === key)
+    return employee.id
+}
+function searchRoleId(key, array){
+    const role = array.find(r => r.title === key)
+    console.log(role.id)
+    return role.id
+}
+async function main(){
+    while (await choices());
+    await closeConnection();
+}
+main()
--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -1,117 +1,63 @@
-const { fetchAsyncQuestionPropertyQuestionProperty } = require('inquirer/lib/utils/utils');
-const con = require('../db/connection')
+//const con = require('../db/connection')
 // roles.title AS role_name FROM employees LEFT JOIN roles ON employees.role_id = roles.id
-function getAllEmployees() { 
-    con.connect( err => {
-    if(err){
-        console.log(err);
-    }
-    const query = (`SELECT e.id, CONCAT(e.first_name, ' ',  e.last_name) AS employee_name, 
-        CONCAT(m.first_name, ' ', m.last_name) AS manager_name, r.title, r.salary, d.department_name
-        FROM employees e
-        LEFT JOIN employees m ON e.manager_id = m.id
-        LEFT JOIN roles r ON e.role_id = r.id
-        LEFT JOIN departments d ON r.department_id = d.id
-         `)
-    con.query(query, (err, result) => {
-        if(err){
-            console.log(err)
-        }
-        console.log(result)
-    })
-})
-}
-function getRoles() {
-    con.connect( err => {
-        if(err){
-            console.log(err);
-        }
-        const query = (`SELECT * FROM roles`)
-        con.query(query, (err, result) => {
-            if(err){
-                console.log(err)
-            }
-            console.log(result)
-        })
-    })
-}
-function getDepartments(){
-    con.connect( err => {
-        if(err){
-            console.log(err);
-        }
-        const query = (`SELECT * FROM departments`)
-        con.query(query, (err, result) => {
-            if(err){
-                console.log(err)
-            }
-            console.log(result)
-        })
-    }) 
-}
-function addDepartment(body){
-    console.log(body);
-    con.connect( err => {
-        if(err){
-            console.log(err);
-        }
-        const query = (`INSERT INTO departments (department_name) VALUES (?)`)
-        const params = body
-        con.query(query, params, (err, result) => {
-            if(err){
-                console.log(err)
-            }
-            console.log('success!')
-        })
-    })  
-}
-function addRole(body) {
-    console.log(body);
-    con.connect( err => {
-        if(err){
-            console.log(err);
-        }
-        const query = (`INSERT INTO roles (title, salary, department_id) VALUES (?,?,?)`)
-        const params = [body.title, body.salary, body.department_id]
-        con.query(query, params, (err, result) => {
-            if(err){
-                console.log(err)
-            }
-            console.log('success!')
-        })
-    })
-}
-function addEmployee(body){
-    console.log(body);
-    con.connect( err => {
-        if(err){
-            console.log(err);
-        }
-        const query = (`INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)`)
-        const params = [body.first_name, body.last_name, body.role_id, body.manger_id]
-        con.query(query, params, (err, result) => {
-            if(err){
-                console.log(err)
-            }
-            console.log('success!')
-        })
-    })
-}
-function changeEmployeeRole(body){
-    console.log(body);
-    con.connect( err => {
-        if(err){
-            console.log(err);
-        }
-        const query = (`UPDATE employees SET role_id = ? WHERE id = ?`)
-        const params = [body.role_id, body.id]
-        con.query(query, params, (err, result) => {
-            if(err){
-                console.log(err)
-            }
-            console.log('success!')
-        })
-    })
-}
+const mysql = require('mysql2/promise');
+require('dotenv').config();
 
-module.exports = {getAllEmployees, getRoles, getDepartments, addDepartment, addRole, addEmployee, changeEmployeeRole}
+  const con = mysql.createConnection({
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME
+});
+async function closeConnection(){
+    const c = await con;
+    c.end();
+}
+async function getAllEmployees() {
+    const c = await con;
+    const query = (`SELECT e.id, CONCAT(e.first_name, ' ',  e.last_name) AS employee_name, 
+    CONCAT(m.first_name, ' ', m.last_name) AS manager_name, r.title, r.salary, d.department_name
+    FROM employees e
+    LEFT JOIN employees m ON e.manager_id = m.id
+    LEFT JOIN roles r ON e.role_id = r.id
+    LEFT JOIN departments d ON r.department_id = d.id;
+    `)
+    return (await c.query(query))[0]
+}
+async function getRoles(){
+    const c = await con;
+    const query = (`SELECT roles.id, roles.title, roles.salary, d.department_name FROM roles LEFT JOIN departments d ON roles.department_id = d.id`)
+    return (await c.query(query))[0]
+}
+async function getDepartments(){
+    const c = await con;
+    const query = (`SELECT * FROM departments`)
+    return (await c.query(query))[0]
+}
+async function addDepartment(body){
+    const c = await con;
+    const query = (`INSERT INTO departments (department_name) VALUES (?)`)
+    const params = body
+    await c.query(query, params) 
+}
+async function addRole(body) {
+    const c = await con;
+    const query = (`INSERT INTO roles (title, salary, department_id) VALUES (?,?,?)`)
+    const params = [body.title, body.salary, body.department_id]
+    await c.query(query, params)
+}
+async function addEmployee(body){
+    const c = await con;
+    const query = (`INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)`)
+    const params = [body.first_name, body.last_name, body.role_id, body.manger_id]
+    await c.query(query, params)
+}
+async function changeEmployeeRole(body){
+    const c = await con;
+    const query = (`UPDATE employees SET role_id = ? WHERE id = ?`)
+    const params = [body.role_id, body.id]
+    await c.query(query, params)
+}
+getRoles()
+
+module.exports = {getAllEmployees, getRoles, getDepartments, addDepartment, addRole, addEmployee, changeEmployeeRole, closeConnection}
